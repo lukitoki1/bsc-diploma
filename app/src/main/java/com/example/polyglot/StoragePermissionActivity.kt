0c2db9d1db1d1cf9d9aad8aaab727019abc9fe14
Łukasz Kamiński <Lukasz.Kaminski@empik.com>
2020-11-08 14:31:31 +0100

    Add app activities boilerplate
    - add app activities structure
    - remove top navbar by changing app theme,
    - add main (welcome) activity layout.

---
+++ app/src/main/java/com/example/polyglot/StoragePermissionActivity.kt
@@ -0,0 +1,11 @@
+package com.example.polyglot
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class StoragePermissionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_storage_permission)
+    }
+}
