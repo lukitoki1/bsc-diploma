ba7fdc422183a95424fa96406bbaa05caec1a666
Łukasz Kamiński <Lukasz.Kaminski@empik.com>
2020-11-08 14:31:31 +0100

    Add app activities boilerplate
    - add app activities structure
    - remove top navbar by changing app theme,
    - add main (welcome) activity layout.

---
+++ app/src/main/java/com/example/polyglot/MainMenuActivity.kt
@@ -0,0 +1,11 @@
+package com.example.polyglot
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainMenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_menu)
+    }
+}
---
+++ app/src/main/java/com/example/polyglot/NoResultsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.polyglot
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class NoResultsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_no_results)
+    }
+}
