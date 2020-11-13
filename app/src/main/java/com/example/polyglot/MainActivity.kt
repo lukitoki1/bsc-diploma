f49ac9cd0efd6fffa0df5c43318e3ef3ed3259c5
Łukasz Kamiński <Lukasz.Kaminski@empik.com>
2020-11-13 20:37:41 +0100

    Implement splash screen

--- app/src/main/java/com/example/polyglot/MainActivity.kt
+++ app/src/main/java/com/example/polyglot/MainActivity.kt
@@ -16,6 +16,7 @@ class MainActivity : AppCompatActivity() {
     private fun startMainMenu() {
         Timer().schedule(1000) {
             startActivity(Intent(applicationContext, MainMenuActivity::class.java))
+            finish()
         }
     }
 }
