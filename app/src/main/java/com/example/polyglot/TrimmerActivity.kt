df7b045304ad59d802384775fc95b36ce249f914
Łukasz Kamiński <Lukasz.Kaminski@empik.com>
2020-11-15 15:39:36 +0100

    Implement image cropping

--- app/src/main/java/com/example/polyglot/TrimmerActivity.kt
+++ app/src/main/java/com/example/polyglot/TrimmerActivity.kt
@@ -1,5 +1,6 @@
 package com.example.polyglot
 
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
@@ -7,19 +8,33 @@ import androidx.appcompat.app.AppCompatActivity
 import com.theartofdev.edmodo.cropper.CropImageView
 
 class TrimmerActivity : AppCompatActivity() {
+    var originalPhotoUri: Uri? = null
+    var trimmerCropView: CropImageView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_trimmer)
-        setImage()
+        initialize()
     }
 
     fun onRejectButtonClick(view: View) {
         finish()
     }
 
-    private fun setImage() {
-        val imageURI = intent.getParcelableExtra<Uri>(PHOTO_URI)
-        val trimmerCropView = findViewById<CropImageView>(R.id.trimmer_crop_view)
-        trimmerCropView.setImageUriAsync(imageURI)
+    fun onAcceptButtonClick(view: View) {
+        trimmerCropView?.getCroppedImageAsync()
+    }
+
+    private fun initialize() {
+        originalPhotoUri = intent.getParcelableExtra(PHOTO_URI)
+        trimmerCropView = findViewById(R.id.trimmer_crop_view)
+        trimmerCropView?.setImageUriAsync(originalPhotoUri)
+        trimmerCropView?.setOnCropImageCompleteListener { _, result -> startResults(result) }
+    }
+
+    private fun startResults(result: CropImageView.CropResult) {
+        startActivity(
+            Intent(this, ResultsActivity::class.java).putExtra(PHOTO_URI, result.uri)
+        )
     }
 }
