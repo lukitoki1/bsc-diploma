3d334deb896b138dfa434c0ec547deca480c6750
Łukasz Kamiński <Lukasz.Kaminski@empik.com>
2020-11-24 22:52:11 +0100

    Separate File Picker activity

---
+++ app/src/main/java/com/example/polyglot/FilePickerActivity.kt
@@ -0,0 +1,73 @@
+package com.example.polyglot
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_file_picker.*
+
+const val READ_STORAGE_PERMISSION = Manifest.permission.READ_EXTERNAL_STORAGE
+
+class FilePickerActivity : AppCompatActivity() {
+    private enum class Views(val value: Int) {
+        LOADING(0),
+        PERMISSION(1)
+    }
+
+    private val storagePermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            when {
+                it -> startFilePicker()
+                else -> showPermissionRationale()
+            }
+        }
+
+    private val filePickerLauncher =
+        registerForActivityResult(ActivityResultContracts.OpenDocument()) { documentUri ->
+            if (documentUri != null) {
+                startActivity(
+                    Intent(this, TrimmerActivity::class.java).putExtra(PHOTO_URI, documentUri)
+                )
+            }
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_file_picker)
+        checkPermission()
+    }
+
+    fun onGrantPermissionButtonClick(view: View) {
+        storagePermissionLauncher.launch(READ_STORAGE_PERMISSION)
+    }
+
+    private fun checkPermission() {
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                READ_STORAGE_PERMISSION
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                startFilePicker()
+            }
+            shouldShowRequestPermissionRationale(READ_STORAGE_PERMISSION) -> {
+                showPermissionRationale()
+            }
+            else -> {
+                storagePermissionLauncher.launch(READ_STORAGE_PERMISSION)
+            }
+        }
+    }
+
+    private fun startFilePicker() {
+        file_picker_view_flipper.displayedChild = Views.LOADING.value
+        filePickerLauncher.launch(arrayOf("image/*"))
+    }
+
+    private fun showPermissionRationale() {
+        file_picker_view_flipper.displayedChild = Views.PERMISSION.value
+    }
+}
--- app/src/main/java/com/example/polyglot/MainMenuActivity.kt
+++ app/src/main/java/com/example/polyglot/MainMenuActivity.kt
@@ -1,41 +1,15 @@
 package com.example.polyglot
 
-import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main_menu.*
 
-const val READ_STORAGE_PERMISSION = Manifest.permission.READ_EXTERNAL_STORAGE
 const val PHOTO_URI = "com.example.polyglot.PHOTO_URI"
 
 class MainMenuActivity : AppCompatActivity() {
-    private val storagePermissionLauncher =
-        registerForActivityResult(
-            ActivityResultContracts.RequestPermission()
-        ) { isGranted: Boolean ->
-            if (isGranted) {
-                startFilePicker()
-            } else {
-                startStoragePermission()
-            }
-        }
-
-    private val chooseFile =
-        registerForActivityResult(ActivityResultContracts.OpenDocument()) { documentUri ->
-            if (documentUri != null) {
-                Log.d("FILES", "chooseFile file URI: $documentUri")
-                startActivity(
-                    Intent(this, TrimmerActivity::class.java).putExtra(PHOTO_URI, documentUri)
-                )
-            }
-        }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
@@ -47,26 +21,7 @@ class MainMenuActivity : AppCompatActivity() {
     }
 
     fun onStartFilePickerButtonClick(view: View) {
-        if (
-            ContextCompat.checkSelfPermission(
-                this,
-                READ_STORAGE_PERMISSION
-            ) == PackageManager.PERMISSION_GRANTED
-        ) {
-            startFilePicker()
-        } else if (shouldShowRequestPermissionRationale(READ_STORAGE_PERMISSION)) {
-            startStoragePermission()
-        } else {
-            storagePermissionLauncher.launch(READ_STORAGE_PERMISSION)
-        }
-    }
-
-    private fun startFilePicker() {
-        chooseFile.launch(arrayOf("image/*"))
-    }
-
-    private fun startStoragePermission() {
-        startActivity(Intent(applicationContext, StoragePermissionActivity::class.java))
+        startActivity(Intent(applicationContext, FilePickerActivity::class.java))
     }
 
     private fun checkCameraFeature() {
